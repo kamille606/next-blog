2d230ab5ce46f377fa94b8d9fbd96b589cec0b78
pengyutao <pengyutao@suzhouyunque.com>
2023-04-19 16:29:07 +0800

    init

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const Header = () => {
+  return (
+    <h1 style={{color: 'red'}}>Header</h1>
+  )
+}
+
+const Footer = () => {
+  return (
+    <h1 style={{color: 'red'}}>Footer</h1>
+  )
+}
+
+export default function App({Component, pageProps}) {
+  return (
+    <>
+      <Header/>
+      <Component {...pageProps} />
+      <Footer/>
+    </>
+  )
 }
---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+import {DatePicker} from "antd";
+import React from "react";
+
+export default function Login() {
+  return (
+    <>
+      <main className={styles.main}>
+        <DatePicker/>
+      </main>
+    </>
+  )
+}
